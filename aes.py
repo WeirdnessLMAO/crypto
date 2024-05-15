5612787cc79c1c926d9448f994bfb862b0f4cfe5
WeirdnessLMAO <142111710+WeirdnessLMAO@users.noreply.github.com>
2024-05-15 20:05:31 +0530

    Update aes.py

--- aes.py
+++ aes.py
@@ -1,14 +1,16 @@
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
+from Crypto.Util.Padding import pad, unpad
 from base64 import b64encode, b64decode
 def aes_encrypt(key, plain_text):
     cipher = AES.new(key, AES.MODE_ECB)
-    cipher_text = cipher.encrypt(plain_text)
+    padded_plain_text = pad(plain_text, AES.block_size)
+    cipher_text = cipher.encrypt(padded_plain_text)
     return b64encode(cipher_text)
 def aes_decrypt(key, cipher_text):
     cipher = AES.new(key, AES.MODE_ECB)
     decrypted_text = cipher.decrypt(b64decode(cipher_text))
-    return decrypted_text
+    return unpad(decrypted_text, AES.block_size)
 def main():
     key = get_random_bytes(16) # Generate a random 16-byte (128-bit) key for AES-128
     plain_text = b"Hello, AES!"
